904a465775d9409db8e17af93d9b2fdb8013d8e1
kumabiko <nousbear.dev@gmail.com>
2023-10-13 13:49:16 +0900

    ♻️ env.d.ts

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -13,14 +13,6 @@ export type Blog = {
   eyecatch?: MicroCMSImage;
 } & MicroCMSDate;
 
-if (!process.env.MICROCMS_SERVICE_DOMAIN) {
-  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
-}
-
-if (!process.env.MICROCMS_API_KEY) {
-  throw new Error("MICROCMS_API_KEY is required");
-}
-
 // API取得用のクライアントを作成
 export const client = createClient({
   serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
