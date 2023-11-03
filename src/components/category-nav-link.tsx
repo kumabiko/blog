14bf9200245747a86950d12198f67099f2019e17
kumabiko <nousbear.dev@gmail.com>
2023-11-03 18:16:10 +0900

    ✨ update category-nav-link

---
+++ src/components/category-nav-link.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { Toggle } from "./ui/toggle";
+
+type Props = {
+  href: string;
+  name: string;
+};
+
+export default function CategoryNavLink({ href, name }: Props) {
+  const pathname = usePathname();
+
+  return (
+    <Toggle variant="outline" size="sm" asChild pressed={pathname === href}>
+      <Link href={href}>{name}</Link>
+    </Toggle>
+  );
+}
