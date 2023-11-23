37b1e9749e35bb9d179430f71a295e8c16425d92
kumabiko <nousbear.dev@gmail.com>
2023-11-23 14:37:00 +0900

    ♻️ refactor: アイコンの置き換え

--- src/components/layout/navbar-item.tsx
+++ src/components/layout/navbar-item.tsx
@@ -5,19 +5,27 @@ import { cn } from "@/lib/utils";
 
 type Props = NavItem & { segment: string | null };
 
-const NavBarItem = ({ href, icon: Icon, label, segment }: Props) => {
+const NavBarItem = ({ href, icons, label, segment }: Props) => {
   const isActive =
     (segment && href.includes(segment)) ||
     (href === "/" && segment === "(blogs)");
+
+  const Icon = isActive ? icons.active : icons.inactive;
+
   return (
     <Link
       className={cn(
-        "flex w-full flex-col items-center gap-x-4 rounded-md px-4 py-3 md:flex-row md:hover:bg-accent",
+        "flex w-full flex-col items-center gap-x-4 rounded-md p-1 md:flex-row md:p-2 md:hover:bg-accent",
       )}
       href={href}
     >
-      <Icon strokeWidth={isActive ? 3 : 2} />
-      <div className={cn("text-lg font-medium", isActive && "font-bold")}>
+      <Icon size={24} />
+      <div
+        className={cn(
+          "text-xs font-medium md:text-lg",
+          isActive && "font-bold",
+        )}
+      >
         {label}
       </div>
     </Link>
--- src/components/layout/tab-bars.tsx
+++ src/components/layout/tab-bars.tsx
@@ -10,13 +10,13 @@ const TabBars = () => {
   const segment = useSelectedLayoutSegment();
 
   return (
-    <div className="fixed bottom-0 z-10 flex w-full translate-y-px justify-around gap-x-2 border-t bg-background/70 p-2 backdrop-blur md:hidden">
+    <div className="fixed bottom-0 z-10 flex w-full translate-y-px justify-around gap-x-2 border-t bg-background/70 backdrop-blur md:hidden">
       {navItems.map((navItem) => (
         <div key={navItem.label}>
           <NavBarItem
             href={navItem.href}
             label={navItem.label}
-            icon={navItem.icon}
+            icons={navItem.icons}
             segment={segment}
           />
         </div>
